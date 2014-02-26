2a5df1e7a51daca9239a5bc279bf08bf35cecf64
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-02-26 15:40:38 +0100

    Updated: small replacement with gzip

--- StreamDecompressor/decompressors/gzip.py
+++ StreamDecompressor/decompressors/gzip.py
@@ -6,5 +6,5 @@ __all__ = ['Gunzip']
 class Gunzip(ExternalPipe):
     __mimes__ = ['application/x-gzip']
     __extensions__ = ['gz']
-    __command__ = 'gunzip -c'.split()
+    __command__ = ['gunzip']
     __compression__ = 'gzip'
