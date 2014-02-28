be3e7d2df768657ebc36036b230174ac468d8663
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-02-28 13:03:24 +0100

    Fixed: 7z decompressor

--- StreamDecompressor/decompressors/p7zip.py
+++ StreamDecompressor/decompressors/p7zip.py
@@ -1,7 +1,7 @@
 import struct
 import binascii
 import re
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, CalledProcessError
 
 from StreamDecompressor import ArchivePack, ArchiveTemp, ExternalPipe
 
@@ -10,46 +10,44 @@ __all__ = ['Un7z']
 
 ereg_header = re.compile('^'+r'--+\n(.+?)(?=\n\n)', re.M+re.S)
 ereg_member = re.compile('^'+r'(.+?)(?=\n\n)', re.M+re.S)
-ereg_info = re.compile(r'^[ \t\f]*(.+?)[ \t\f]*=[ \t\f]*(.*?)[ \t\f]*$', re.M)
 
 
-class InfoBase:
-    def _normalize_key(self, key):
-        return re.sub(r'\W', '_', key.lower())
-
-    def __init__(self, infostring):
-        if hasattr(self, '_base_attributes'):
-            for attr in self._base_attributes:
-                setattr(self, attr, None)
-        for m in ereg_info.finditer(infostring):
-            setattr(self, self._normalize_key(m.group(1)), m.group(2))
-        if hasattr(self, '_expected'):
-            assert set(self._expected) - set(dir(self)) == set(), \
-                "Some information are missing in info:\n" + infostring
+def parse_hunk(hunk):
+    info = {}
+    for m in re.finditer(
+            r'^[ \t\f]*(.+?)[ \t\f]*=[ \t\f]*(.*?)[ \t\f]*$',
+            hunk, flags=re.M):
+        key = re.sub(r'\W', '_', m.group(1).lower())
+        info[key] = m.group(2)
+    return info
 
 
-class Header(InfoBase):
-    _expected = "path type solid blocks headers_size "\
-                "method physical_size".split()
-
-    def __init__(self, infostring):
-        InfoBase.__init__(self, infostring)
-        self.physical_size = int(self.physical_size)
-        self.headers_size = int(self.headers_size)
-        self.blocks = int(self.blocks)
+class Header(object):
+    def __init__(self, hunk):
+        info = parse_hunk(hunk)
+        info['physical_size'] = int(info['physical_size'])
+        info['headers_size'] = int(info['headers_size'])
+        info['blocks'] = int(info['blocks'])
+        self.__dict__.update(info)
 
 
-class Member(InfoBase):
-    _expected = "path size modified attributes crc encrypted "\
-                "method packed_size".split()
+class Member(object):
+    def __init__(self, hunk):
+        info = parse_hunk(hunk)
+        info['filename'] = info['path']
+        info['size'] = int(info['size'])
+        info['packed_size'] = int(info['packed_size'] or '0')
+        info['block'] = int(info['block'] or '0')
+        if info['crc']:
+            info['crc'] = reduce(lambda x, y: x * 256 + y, \
+                struct.unpack('BBBB', binascii.unhexlify(info['crc'])), 0)
+        self.__dict__.update(info)
 
-    def __init__(self, infostring):
-        InfoBase.__init__(self, infostring)
-        self.size = int(self.size)
-        self.packed_size = int(self.packed_size or '0')
-        self.block = int(self.block or '0')
-        self.crc = reduce(lambda x, y: x * 256 + y, \
-            struct.unpack('BBBB', binascii.unhexlify('B53C0674')), 0)
+    def isfile(self):
+        return self.attributes[0] != 'D'
+
+    def isdir(self):
+        return self.attributes[0] == 'D'
 
 
 class Un7z(ArchivePack):
@@ -72,10 +70,7 @@ class Un7z(ArchivePack):
                         for m in ereg_member.finditer(info,
                             re.search('^'+'-'*10+'$', info, re.M).end(0))]
         if len(self._members) == 1:
-            self.p = Popen(self.__command__ + ['e', self.fileobj.name, '-so'],
-                           stdout=PIPE, stderr=PIPE)
-            self.p.stderr.close()
-            stream = self.p.stdout
+            stream = self.open(self._members[0], stream=True)
         else:
             stream = self.fileobj
             stream.seek(0)
@@ -87,34 +82,34 @@ class Un7z(ArchivePack):
     def open(self, member, stream=True):
         p = Popen(self.__command__ +
             ['e', self.fileobj.name, '-so',
-            (member.path if isinstance(member, Member) else member)],
+            (member.filename if isinstance(member, Member) else member)],
             stdout=PIPE, stderr=PIPE)
-        p.stderr.close()
         if stream:
             return p.stdout
         else:
             temp = ArchiveTemp(p.stdout)
-            ret = p.wait()
-            if ret != 0:
-                raise Exception("Process returned with value=%d" % ret)
+            retcode = p.wait()
+            if retcode:
+                raise CalledProcessError(
+                    retcode, self.__command__, output=p.stderr.read())
             return temp
 
     def extract(self, member, path):
         p = Popen(self.__command__ +
-            ['e', self.fileobj.name, '-y', '-o'+path,
-            (member.path if isinstance(member, Member) else member)],
+            ['x', self.fileobj.name, '-y', '-o'+path,
+            (member.filename if isinstance(member, Member) else member)],
             stdout=PIPE)
-        p.stdout.close()
-        ret = p.wait()
-        if ret != 0:
-            raise Exception("Process returned with value=%d" % ret)
+        retcode = p.wait()
+        if retcode:
+            raise CalledProcessError(
+                retcode, self.__command__, output=p.stdout.read())
 
     def extractall(self, path, members=[]):
         p = Popen(self.__command__ +
             ['x', self.fileobj.name, '-y', '-o'+path] +
-            [(m.path if isinstance(m, Member) else m) for m in members],
+            [(m.filename if isinstance(m, Member) else m) for m in members],
             stdout=PIPE)
-        p.stdout.close()
-        ret = p.wait()
-        if ret != 0:
-            raise Exception("Process returned with value=%d" % ret)
+        retcode = p.wait()
+        if retcode:
+            raise CalledProcessError(
+                retcode, self.__command__, output=p.stdout.read())
