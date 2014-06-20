7a8e253f700299ccba92e6a58a5bff539835d4a3
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-06-20 14:02:42 +0200

    Fixed: missing mime for gzip compression

--- StreamDecompressor/decompressors/gzip.py
+++ StreamDecompressor/decompressors/gzip.py
@@ -4,7 +4,10 @@ __all__ = ['Gunzip']
 
 
 class Gunzip(ExternalPipe):
-    __mimes__ = ['application/x-gzip']
+    __mimes__ = [
+        'application/x-gzip',
+        'application/gzip',
+    ]
     __extensions__ = ['gz']
     __command__ = ['gunzip']
     __compression__ = 'gzip'
