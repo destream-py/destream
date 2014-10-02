3acb1ddf730a53933d971378657ad77a8cf0369c
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-02 12:35:36 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "StreamDecompressor",
+    version = "1.0",
+    author = "Cecile Tonglet",
+    author_email = "cecile.tonglet@gmail.com",
+    description = ("A simple module to decompress streams compressed multiple "
+                   "times"),
+    license = "GPLv2",
+    keywords = "postgres database dump restore",
+    url = "https://github.com/cecton/StreamDecompressor",
+    packages = find_packages(),
+    install_requires = ['StreamDecompressor'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: System :: Archiving :: Compression",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+)
