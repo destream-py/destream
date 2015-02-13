6d624a188ce51cb41f2ad834f2ab1da8d4a340c3
Cécile Tonglet <cecile.tonglet@gmail.com>
2015-02-13 15:10:17 +0100

    Fixed: missing dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "StreamDecompressor",
-    version = "2.1",
+    version = "2.1-1",
     author = "Cecile Tonglet",
     author_email = "cecile.tonglet@gmail.com",
     description = ("A simple module to decompress streams compressed multiple "
@@ -11,7 +11,7 @@ setup(
     keywords = "postgres database dump restore",
     url = "https://github.com/cecton/StreamDecompressor",
     packages = find_packages(),
-    install_requires = ['StreamDecompressor'],
+    install_requires = ['StreamDecompressor', 'python-magic'],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Topic :: System :: Archiving :: Compression",
