ea207bb4cb21469dc177b615ef2cc934d1bbe908
Cecile Tonglet <cecile.tonglet@gmail.com>
2016-07-28 21:16:57 +0200

    Upgrade to python-magic 0.4.12 (#4)
    Fixes #2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url = "https://github.com/cecton/destream",
     packages = find_packages(),
     scripts = ['scripts/destream'],
-    install_requires = ['python-magic==0.4.11'],
+    install_requires = ['python-magic==0.4.12'],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Topic :: System :: Archiving :: Compression",
