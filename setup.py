a9b3546c0fbd5ac4e0c6c11eb8d896b29fd1b7c1
Cecile Tonglet <cecile.tonglet@cecton.com>
2019-09-13 12:34:44 +0200

    Increase version and update dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "destream",
-    version = "4.2.0",
+    version = "5.0.0",
     author = "Cecile Tonglet",
     author_email = "cecile.tonglet@gmail.com",
     description = ("A simple module to decompress streams compressed multiple "
@@ -12,7 +12,7 @@ setup(
     url = "https://github.com/cecton/destream",
     packages = find_packages(),
     scripts = ['scripts/destream'],
-    install_requires = ['python-magic==0.4.12'],
+    install_requires = ['python-magic>=0.4.12'],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Topic :: System :: Archiving :: Compression",
